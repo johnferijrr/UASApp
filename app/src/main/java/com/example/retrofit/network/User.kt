77509e3473b852019c2ae98d85630f6ba0fd7899
johnferijrr <112480086+johnferijrr@users.noreply.github.com>
2024-12-15 23:08:25 +0700

    Update User.kt

--- app/src/main/java/com/example/retrofit/network/User.kt
+++ app/src/main/java/com/example/retrofit/network/User.kt
@@ -5,8 +5,8 @@ data class User(
     val username: String? = null,
     val password: String? = null,
     val data1: String? = null,
-    val data2: String? = null, // Now data2 is a string
-    val data3: String? = null  // data3 is already a string
+    val data2: String? = null,
+    val data3: String? = null
 )
 
 
